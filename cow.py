a086746e8aeb1bbb5d9a68123cf5f3de4e1ffd97
Richie Glennon <glennonr@moravian.edu>
2022-02-02 14:53:48 -0500

    adds cow

--- cow.py
+++ cow.py
@@ -5,5 +5,11 @@ def moo():
 	for i in range(100):
 		print('MOO!')
 
+def make_cow():
+	cow = ["          \\  ^__^","           \\ (oo)\\________","             (__)\\        )\\/\\","                  ||----W |","                  ||     ||"]
+	for line in cow:
+		print(line)
+
 if __name__ == '__main__':
 	moo()
+	make_cow()
